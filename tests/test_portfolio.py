2369a0a005276c5551849cab94fca0c5329104f4
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-04-23 22:18:02 -0700

    test add

--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -58,3 +58,25 @@ def test_cash(portfolio):
     assert portfolio.equity.sum(axis=1).diff().cumsum().values[-1] == pytest.approx(17665.06)
 
 
+def test_add(prices, resource_dir):
+    """
+    Tests the addition of two portfolios
+    TODP: Currently only tests the positions of the portfolios
+    """
+    index_left = pd.DatetimeIndex([pd.Timestamp("2013-01-01"), pd.Timestamp("2013-01-02")])
+    index_right = pd.DatetimeIndex([pd.Timestamp("2013-01-02"), pd.Timestamp("2013-01-03"), pd.Timestamp("2013-01-04")])
+
+    pos_left = pd.DataFrame(data={"A": [0, 1], "C": [3, 3]}, index=index_left)
+    pos_right = pd.DataFrame(data={"A": [1, 1, 2], "B": [2, 3, 4]}, index=index_right)
+
+    print(pos_left)
+    print(pos_right)
+    port_left = build_portfolio(prices, stocks=pos_left)
+    port_right = build_portfolio(prices, stocks=pos_right)
+
+    pd.testing.assert_frame_equal(pos_left, port_left.stocks)
+    pd.testing.assert_frame_equal(pos_right, port_right.stocks)
+
+    port_add = port_left + port_right
+    www = pd.read_csv(resource_dir / "positions.csv", index_col=0, parse_dates=[0])
+    pd.testing.assert_frame_equal(www, port_add.stocks, check_freq=False)
