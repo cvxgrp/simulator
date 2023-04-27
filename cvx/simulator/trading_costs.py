b928bf7333e6a2f3d9543b2e69c159798877afb4
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-04-27 10:09:15 -0700

    scipy notebook (#29)

--- cvx/simulator/trading_costs.py
+++ cvx/simulator/trading_costs.py
@@ -1,15 +1,13 @@
 from dataclasses import dataclass
 import abc
 
-import pandas as pd
-
 
 @dataclass(frozen=True)
 class TradingCostModel(abc.ABC):
     name: str
 
     @abc.abstractmethod
-    def eval(self, trades_volume, trades_stocks) -> pd.DataFrame:
+    def eval(self, prices, trades):
         pass
 
 
@@ -18,5 +16,6 @@ class LinearCostModel(TradingCostModel):
     factor: float = 0.0
     bias: float = 0.0
 
-    def eval(self, trades_volume, trades_stocks):
-        return self.factor * trades_volume.abs() + self.bias * trades_stocks.abs()
+    def eval(self, prices, trades):
+        volume = prices * trades
+        return self.factor * volume.abs() + self.bias * trades.abs()
