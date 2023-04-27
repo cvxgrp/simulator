c6052f11155aac03085d4bffc626a896d5d86acd
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-04-27 15:34:29 -0700

    examples

--- examples/thomas.py
+++ examples/thomas.py
@@ -2,13 +2,15 @@ import pandas as pd
 pd.options.plotting.backend = "plotly"
 
 import yfinance as yf
+import quantstats as qs
 
 from cvx.simulator.portfolio import build_portfolio
+from cvx.simulator.metrics import Metrics
 
 if __name__ == '__main__':
 
     data = yf.download(tickers = "SPY AAPL GOOG MSFT",  # list of tickers
-                       period = "10y",                   # time period
+                       period = "10y",                  # time period
                        interval = "1d",                 # trading interval
                        prepost = False,                 # download pre/post market hours data?
                        repair = True)                   # repair obvious price errors e.g. 100x?
@@ -20,7 +22,21 @@ if __name__ == '__main__':
 
     for _, now, snapshot in portfolio:
         # each day we invest a quarter of the capital in the assets
-        portfolio[now] = 0.25 * snapshot.nav / snapshot.prices
+        portfolio[now] = 0.125 * snapshot.nav / snapshot.prices
 
-    fig = portfolio.cash.plot()
-    fig.show()
+    #fig = portfolio.cash.plot()
+    #fig.show()
+
+    qs.plots.snapshot(portfolio.nav, title="1/n portfolio")
+    print(Metrics(portfolio.profit).sr_profit)
+
+    # show sharpe ratio
+    print(qs.stats.sharpe(portfolio.nav.pct_change()))
+    print(qs.stats.sharpe(portfolio.profit))
+    print(portfolio.nav.min())
+    print(portfolio.nav.max())
+    print(qs.stats.sharpe(portfolio.nav))
+
+    print(Metrics(portfolio.nav.pct_change().dropna()).sr_profit)
+    print(qs.stats.sharpe(portfolio.nav.pct_change().dropna()))
+    print(qs.stats.sharpe(portfolio.nav))
