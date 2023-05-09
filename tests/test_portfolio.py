28cff8652937142279fa65eb7a977c8899994f97
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-05-08 23:09:43 -0700

    check monotonicity of index

--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -200,3 +200,42 @@ def test_set_cashpositions(prices):
     assert portfolio.nav.values[-1] == pytest.approx(49773.093729)
 
         #portfolio[now] = portfolio[before]
+
+def test_duplicates():
+    """
+    duplicate in index
+    """
+    prices = pd.DataFrame(index=[1, 1], columns=["A"])
+    with pytest.raises(AssertionError):
+        build_portfolio(prices=prices)
+
+    prices = pd.DataFrame(index=[1], columns=["A"])
+    position = pd.DataFrame(index=[1, 1], columns=["A"])
+
+    with pytest.raises(AssertionError):
+        build_portfolio(prices=prices, stocks=position)
+
+
+def test_monotonic():
+    """
+    index not increasing
+    """
+    prices = pd.DataFrame(index=[2, 1], columns=["A"])
+    with pytest.raises(AssertionError):
+        build_portfolio(prices=prices)
+
+def test_portfolio(prices):
+    """
+    build portfolio from price
+    """
+    portfolio = build_portfolio(prices=prices)
+    pd.testing.assert_frame_equal(portfolio.prices, prices.ffill())
+
+    for t in portfolio.index:
+        # set the position
+        portfolio[t] = pd.Series(index=prices.keys(), data=1000.0)
+
+    pd.testing.assert_frame_equal(
+        portfolio.stocks,
+        pd.DataFrame(index=prices.index, columns=prices.keys(), data=1000.0),
+    )
