85b76247dec7ba29183b4a805c504607393f4f35
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-08-18 23:23:20 -0700

    startup fixed

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -2,5 +2,5 @@
 pipx install poetry
 poetry config virtualenvs.in-project true
 poetry install
+poetry run pip install ipykernel pre-commit
 poetry run pre-commit install
-poetry run pip install ipykernel
