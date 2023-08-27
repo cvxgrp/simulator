3253ecd8ee41116f3ef6a54a5da501e6dd3bfcfb
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-08-27 12:04:40 -0700

    exclude windows 3.11

--- tests/test_trading_costs.py
+++ tests/test_trading_costs.py
@@ -1,22 +1,18 @@
-# # -*- coding: utf-8 -*-
-# from __future__ import annotations
-#
-# import pandas as pd
-#
-# from cvx.simulator.trading_costs import LinearCostModel
-#
-#
-# def test_linear_model():
-#     """
-#     Test the linear cost model
-#     """
-#     trades = pd.Series(index=["A", "B"], data=[20.0, -10.0])
-#     prices = pd.Series(index=["A", "B"], data=[100.0, 300.0])
-#
-#     model = LinearCostModel(factor=0.0010, bias=0.5)
-#     x = model.eval(prices=prices, trades=trades).sum()
-#     assert x == 5.0 + 15.0
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+import pandas as pd
+
+from cvx.simulator.trading_costs import LinearCostModel
 
 
-def test_trivial():
-    assert True
+def test_linear_model():
+    """
+    Test the linear cost model
+    """
+    trades = pd.Series(index=["A", "B"], data=[20.0, -10.0])
+    prices = pd.Series(index=["A", "B"], data=[100.0, 300.0])
+
+    model = LinearCostModel(factor=0.0010, bias=0.5)
+    x = model.eval(prices=prices, trades=trades).sum()
+    assert x == 5.0 + 15.0
