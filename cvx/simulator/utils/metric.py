94bbeef1f2ad7efdf6430bddb8ae12c8eaf713f9
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2024-11-09 07:58:26 +0800

    sharpe for constant series

--- cvx/simulator/utils/metric.py
+++ cvx/simulator/utils/metric.py
@@ -1,19 +1,21 @@
-from math import sqrt
-
 import pandas as pd
-
+import numpy as np
 
 def _periods(ts):
     """
     compute the number of periods in a time series
     """
     series = pd.Series(data=ts.index)
-    return 365 * 24 * 60 * 60 / (series.diff().mean().total_seconds())
+    return 365 * 24 * 60 * 60 / (series.diff().dropna().mean().total_seconds())
 
 
 def sharpe(ts, n=None):
     """
     compute the sharpe ratio of a time series
     """
-    n = n or _periods(ts)
-    return ts.mean() / ts.std() * sqrt(n)
+    std = ts.std()
+    if std > 0:
+        n = n or _periods(ts)
+        return (ts.mean() / std) * np.sqrt(n)
+    else:
+        return np.inf
