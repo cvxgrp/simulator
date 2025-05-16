96f4852a5cbb87637b33793482c281f13217c37b
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-05-16 12:36:29 +0400

    metric

--- src/tests/test_utils/test_metric.py
+++ src/tests/test_utils/test_metric.py
@@ -1,9 +1,6 @@
 import pytest
 
-from cvx.simulator.utils.metric import sharpe
 
-
-def test_sharpe(nav):
-    s = sharpe(nav.pct_change())
-    print(s)
-    assert s == pytest.approx(-0.10388478316042028)
+def test_sharpe(data) -> None:
+    s = data.stats.sharpe()
+    assert s["NAV"] == pytest.approx(-0.10388478316042028)
