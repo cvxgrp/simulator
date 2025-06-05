7a2accc7b93238bc61c1401b3986e422e9ea019f
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-06-05 23:35:51 +0400

    testing reproduction of loaded frames

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from pathlib import Path
 
 import pandas as pd
+import polars as pl
 import pytest
 
 
@@ -17,7 +18,19 @@ def resource_fixture():
 @pytest.fixture()
 def prices(resource_dir):
     """prices fixture"""
-    return pd.read_csv(resource_dir / "price.csv", index_col=0, parse_dates=True, header=0).ffill()
+    frame = pd.read_csv(resource_dir / "price.csv", index_col=0, parse_dates=True, header=0).ffill()
+    frame.index.name = "date"
+    return frame
+
+
+@pytest.fixture()
+def prices_pl(resource_dir):
+    """prices_pl fixture"""
+    frame = pl.read_csv(resource_dir / "price.csv", try_parse_dates=True)
+    frame = frame.with_columns(pl.col("date").cast(pl.Datetime("ns")))
+    return frame.fill_null(strategy="forward")
+
+    # return frame
 
 
 @pytest.fixture()
