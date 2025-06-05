3ea75c8d10d0580ccbd1516339f5ca23c8bf2931
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-06-05 16:44:14 +0400

    notebooks without path construction

--- book/marimo/pairs.py
+++ book/marimo/pairs.py
@@ -1,52 +1,50 @@
 import marimo
 
-__generated_with = "0.9.27"
+__generated_with = "0.13.15"
 app = marimo.App()
 
 
 @app.cell
-def __(mo):
+def _(mo):
     mo.md(r"""# Almost pairs trading""")
     return
 
 
 @app.cell
-def __(mo):
+def _(mo):
     mo.md(
         r"""
-        This little exercise goes back to an idea by Stephen Boyd:
+    This little exercise goes back to an idea by Stephen Boyd:
 
-        The simulator should always be completely agnostic as to the trading policy.
-        You should even demonstrate this with silly policies.
-        Like here’s one:  Each day choose names from the universe at random.
-        Buy one (say 0.1 of your portfolio wealth) and short one the same amount.
-        Not a good strategy, but a valid one.
-        Of course the simulate will terminate if you go bust (which seems likely).
-        """
+    The simulator should always be completely agnostic as to the trading policy.
+    You should even demonstrate this with silly policies.
+    Like here’s one:  Each day choose names from the universe at random.
+    Buy one (say 0.1 of your portfolio wealth) and short one the same amount.
+    Not a good strategy, but a valid one.
+    Of course the simulate will terminate if you go bust (which seems likely).
+    """
     )
     return
 
 
 @app.cell
-def __(__file__):
-    from pathlib import Path
-
+def _():
+    import marimo as mo
     import numpy as np
     import pandas as pd
     from loguru import logger
 
     from cvx.simulator import Builder
 
-    pd.options.plotting.backend = "plotly"
-    folder = Path(__file__).parent
-
-    return Builder, Path, folder, logger, np, pd
+    return Builder, logger, mo, np, pd
 
 
 @app.cell
-def __(Builder, folder, logger, np, pd):
+def _(Builder, logger, mo, np, pd):
     logger.info("Load prices")
-    prices = pd.read_csv(folder / "data" / "stock-prices.csv", index_col=0, parse_dates=True, header=0)
+    prices = pd.read_csv(
+        str(mo.notebook_location() / "data" / "stock-prices.csv"), index_col=0, parse_dates=True, header=0
+    )
 
     logger.info("Build portfolio")
     b = Builder(prices=prices, initial_aum=1e6)
@@ -67,20 +65,13 @@ def __(Builder, folder, logger, np, pd):
         b.aum = state.aum - costs
 
     portfolio = b.build()
-    return b, costs, pair, portfolio, prices, state, t, units
+    return (portfolio,)
 
 
 @app.cell
-def __(portfolio):
+def _(portfolio):
     portfolio.nav.plot()
     return
-
-
-@app.cell
-def __():
-    import marimo as mo
-
-    return (mo,)
 
 
 if __name__ == "__main__":
